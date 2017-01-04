155c4014415349076c0f96f34709c381ac35ed56
Jmac217 <jordanelder10@gmail.com>
2017-01-04 17:42:00 -0600

    Beutified jQuery

--- my-simple-portfolio/main.js
+++ my-simple-portfolio/main.js
@@ -1,20 +1,31 @@
-$(document).ready(function (){
-  var links = [
-    {"title": "Home", "head": "Readable Text!", "body":"This HTML page contains no inline styles! The CSS contains many useful semantics that should be insightful when piecing the whole picture together."},
-    {"title": "About Me", "head": "About Me", "body": "Dolorum nobis nostrum perferendis recusandae delectus accusamus rerum tempore, voluptates soluta. Quaerat omnis explicabo aliquam at, minima rem, quod aut consectetur velit."},
-    {"title": "Contact", "head": "Contact", "body": "Similique non saepe velit labore pariatur, quo iusto in id excepturi, debitis tempora, porro suscipit commodi explicabo mollitia quaerat impedit ex praesentium."},
-  ];
-  $("a").click(function (){
-    for (let i of links){
-      if(i.title===$(this).text()){
-        $("#content").html(`
+$(document).ready(function() {
+    var links = [{
+            "title": "Home",
+            "head": "Readable Text!",
+            "body": "This HTML page contains no inline styles! The CSS contains many useful semantics that should be insightful when piecing the whole picture together."
+        },
+        {
+            "title": "About Me",
+            "head": "About Me",
+            "body": "Dolorum nobis nostrum perferendis recusandae delectus accusamus rerum tempore, voluptates soluta. Quaerat omnis explicabo aliquam at, minima rem, quod aut consectetur velit."
+        },
+        {
+            "title": "Contact",
+            "head": "Contact",
+            "body": "Similique non saepe velit labore pariatur, quo iusto in id excepturi, debitis tempora, porro suscipit commodi explicabo mollitia quaerat impedit ex praesentium."
+        },
+    ];
+    $("a").click(function() {
+        for (let i of links) {
+            if (i.title === $(this).text()) {
+                $("#content").html(`
           <div class="card">
-            <h2>`+i.title+`</h2>
-            <h3>`+i.head+`</h3>
-            <p>`+i.body+`</p>
+            <h2>` + i.title + `</h2>
+            <h3>` + i.head + `</h3>
+            <p>` + i.body + `</p>
           </div>
         `);
-      }
-    }
-  });
+            }
+        }
+    });
 });
